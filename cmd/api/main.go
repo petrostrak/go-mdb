bc07a74a98f7fab5938b26d1ed8b2356d3696a9c
petrostrak <pit.trak@gmail.com>
2023-12-05 20:30:01 +0200

    add healthcheck endpoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,56 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+
+// A config struct that holds all the configuration settinfs for the application.
+type config struct {
+	port int
+	env  string
+}
+
+// An application struct that holds all the dependencies for the HTTP handlers,
+// helpers and middlewares.
+type application struct {
+	config config
+	logger *log.Logger
+}
+
 func main() {
-	println("Hello, world!")
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Second,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	if err := srv.ListenAndServe(); err != nil {
+		logger.Fatal(err)
+	}
 }
