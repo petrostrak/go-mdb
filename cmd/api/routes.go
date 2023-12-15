b91b6af643d89c400cfab7053a38bb2667a472ca
petrostrak <pit.trak@gmail.com>
2023-12-15 19:33:32 +0200

    panic recovery middleware

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -6,7 +6,7 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func (app *application) routes() *httprouter.Router {
+func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
@@ -19,5 +19,5 @@ func (app *application) routes() *httprouter.Router {
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMoviesHandler)
 
-	return router
+	return app.recoverPanic(router)
 }
