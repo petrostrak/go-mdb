04224a91914986dd882a2776fbd15e2aba55f6ac
petrostrak <pit.trak@gmail.com>
2023-12-16 12:28:54 +0200

    Catching SIGINT and SIGTERM signals

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -14,6 +17,23 @@ func (app *application) serve() error {
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
+
+	go func() {
+		quit := make(chan os.Signal, 1)
+
+		// Listen for incoming SIGINT and SIGTERM signals and relay them to
+		// the quit channel.
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		// Read the signal from the quit channel. This code will block until
+		// a signal is received.
+		s := <-quit
+
+		app.logger.PrintInfo("caugth signal", map[string]string{
+			"signal": s.String(),
+		})
+		os.Exit(0)
+	}()
 
 	app.logger.PrintInfo("starting server", map[string]string{
 		"addr": srv.Addr,
