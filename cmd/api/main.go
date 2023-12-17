015e42b8d2a5c7334aee9ebb614c5240aee3b760
petrostrak <pit.trak@gmail.com>
2023-12-17 22:28:27 +0200

    Use mail helper

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,6 +10,7 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/petrostrak/gomdb/internal/data"
 	"github.com/petrostrak/gomdb/internal/jsonlog"
+	"github.com/petrostrak/gomdb/internal/mailer"
 )
 
 const version = "1.0.0"
@@ -29,6 +30,13 @@ type config struct {
 		burst   int
 		enabled bool
 	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
 }
 
 // An application struct that holds all the dependencies for the HTTP handlers,
@@ -37,6 +45,7 @@ type application struct {
 	config config
 	logger *jsonlog.Logger
 	models data.Models
+	mailer mailer.Mailer
 }
 
 func main() {
@@ -55,6 +64,12 @@ func main() {
 	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
 	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
 
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 25, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "db53a10bf87bbf", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "64d1e075467206", "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Go-MDB <no-reply@gomdb.petros_trak.net>", "SMTP sender")
+
 	flag.Parse()
 
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
@@ -70,6 +85,7 @@ func main() {
 		config: cfg,
 		logger: logger,
 		models: data.NewModels(db),
+		mailer: mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender),
 	}
 
 	err = app.serve()
