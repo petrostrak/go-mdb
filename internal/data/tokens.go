e4eff53eae9d18ce6567b595eb76efb8aeb1427a
petrostrak <pit.trak@gmail.com>
2023-12-19 15:58:18 +0200

    Add Token struck and generateToken function

---
+++ internal/data/tokens.go
@@ -0,0 +1,52 @@
+package data
+
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base32"
+	"time"
+)
+
+const ScopeActivation = "activation"
+
+// A Token struct to hold the data for an individual token
+type Token struct {
+	Plaintext string
+	Hash      []byte
+	UserID    int64
+	Expiry    time.Time
+	Scope     string
+}
+
+func generateToken(userID int64, ttl time.Duration, scope string) (*Token, error) {
+	token := &Token{
+		UserID: userID,
+		Expiry: time.Now().Add(ttl),
+		Scope:  scope,
+	}
+
+	randomBytes := make([]byte, 16)
+
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	// Encode the byte slice to a base-32-encoded string and assign it to the token
+	// Plaintext field. This will be the token string that we send to the user in their
+	// welcome email. They will look similar to this:
+	//
+	// Y3QMGX3PJ3WLRL2YRTQGQ6KRHU
+	//
+	// Note that by default base-32 strings may be padded at the end with the =
+	// character. We don't need this padding character for the purpose of our tokens, so
+	// we use the WithPadding(base32.NoPadding) method in the line below to omit them.
+	token.Plaintext = base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(randomBytes)
+
+	// Generate a SHA-256 hash of the plaintext token string. This will be the value
+	// that we store in the `hash` field of our database table.
+	hash := sha256.Sum256([]byte(token.Plaintext))
+	token.Hash = hash[:]
+
+	return token, nil
+}
