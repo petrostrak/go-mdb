f07a3a6f4c6629f4e282ad1d8a168469e5394ae2
petrostrak <pit.trak@gmail.com>
2023-12-19 18:45:02 +0200

    add deleteAllForUser

--- internal/data/tokens.go
+++ internal/data/tokens.go
@@ -64,6 +64,8 @@ type TokenModel struct {
 	DB *sql.DB
 }
 
+// The New method is a shortcut which creates a new Token struct and then inserts the
+// data in the tokens table.
 func (m *TokenModel) New(userID int64, ttl time.Duration, scope string) (*Token, error) {
 	token, err := generateToken(userID, ttl, scope)
 	if err != nil {
@@ -74,6 +76,7 @@ func (m *TokenModel) New(userID int64, ttl time.Duration, scope string) (*Token,
 	return token, err
 }
 
+// // Insert adds the data for a specific token to the tokens table.
 func (m *TokenModel) Insert(token *Token) error {
 	query := `
 		INSERT INTO tokens (hash, user_id, expiry, scope)
@@ -85,5 +88,18 @@ func (m *TokenModel) Insert(token *Token) error {
 	defer cancel()
 
 	_, err := m.DB.ExecContext(ctx, query, args...)
+	return err
+}
+
+// // DeleteAllForUser deletes all tokens for a specific user and scope.
+func (m TokenModel) DeleteAllForUser(scope string, userID int64) error {
+	query := `
+		DELETE FROM tokens
+		WHERE scope = $1 AND user_id = $2`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, query, userID)
 	return err
 }
