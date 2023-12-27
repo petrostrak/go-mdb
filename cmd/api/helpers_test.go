adbdddb77a2d6ac3b5f55dcee455fe2886ed5aeb
petrostrak <pit.trak@gmail.com>
2023-12-27 12:26:59 +0200

    Add Test_readJSON

--- cmd/api/helpers_test.go
+++ cmd/api/helpers_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -53,5 +55,49 @@ func Test_writeJSON(t *testing.T) {
 	err := app.writeJSON(rr, http.StatusOK, payload, headers)
 	if err != nil {
 		t.Errorf("failed to write JSON: %v\n", err)
+	}
+}
+
+func Test_readJSON(t *testing.T) {
+	var app application
+
+	sampleJSON := map[string]any{
+		"foo": "bar",
+	}
+	body, _ := json.Marshal(sampleJSON)
+
+	var decodedJSON struct {
+		Foo string `json:"foo"`
+	}
+
+	req, err := http.NewRequest("POST", "/", bytes.NewReader(body))
+	if err != nil {
+		t.Log("Error", err.Error())
+	}
+
+	rr := httptest.NewRecorder()
+	defer req.Body.Close()
+
+	err = app.readJSON(rr, req, &decodedJSON)
+	if err != nil {
+		t.Error("failed to decode json", err)
+	}
+
+	badJSON := `
+		{
+			"foo": "bar"
+		}
+		{
+			"alpha": "beta"
+		}`
+
+	req, err = http.NewRequest("POST", "/", bytes.NewReader([]byte(badJSON)))
+	if err != nil {
+		t.Log("Error", err)
+	}
+
+	err = app.readJSON(rr, req, &decodedJSON)
+	if err == nil {
+		t.Error("did not get an error with bad json")
 	}
 }
