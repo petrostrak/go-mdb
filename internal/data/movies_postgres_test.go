9829b71794e57a799f23dba2097d39d7fa05c752
petrostrak <pit.trak@gmail.com>
2023-12-29 12:07:45 +0200

    add createTables for migration tests

--- internal/data/movies_postgres_test.go
+++ internal/data/movies_postgres_test.go
@@ -15,10 +15,10 @@ import (
 var (
 	host   = "localhost"
 	user   = "postgres"
-	pw     = "password"
-	dbName = "movie_test"
+	pw     = "postgres"
+	dbName = "movies_test"
 	port   = "5435"
-	dns    = "host=%s port=%s user=%s password=%s dbname=%s sslmod=disabled timezone=UTC connect_timeout=5"
+	dsn    = "host=%s port=%s user=%s password=%s dbname=%s sslmode=disable timezone=UTC connect_timeout=5"
 )
 
 var resource *dockertest.Resource
@@ -26,14 +26,16 @@ var pool *dockertest.Pool
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
+	// connect to docker; fail if docker not running
 	p, err := dockertest.NewPool("")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("could not connect to docker; is it running? %s", err)
 	}
 
 	pool = p
 
-	opts := dockertest.RunOptions{
+	// set up our docker options, specifying the image and so forth
+	options := dockertest.RunOptions{
 		Repository: "postgres",
 		Tag:        "14",
 		Env: []string{
@@ -49,25 +51,55 @@ func TestMain(m *testing.M) {
 		},
 	}
 
-	resource, err := pool.RunWithOptions(&opts)
+	// get a resource (docker image)
+	resource, err = pool.RunWithOptions(&options)
 	if err != nil {
-		pool.Purge(resource)
-		log.Fatal(err)
+		_ = pool.Purge(resource)
+		log.Fatalf("could not start resource: %s", err)
 	}
 
-	if err := pool.Retry(func() error {
-		var err error
-		testDB, err = sql.Open("pq", fmt.Sprintf(dns, host, port, user, pw, dbName))
+	// start the image and wait until it's ready
+	if err = pool.Retry(func() error {
+		testDB, err = sql.Open("postgres", fmt.Sprintf(dsn, host, port, user, pw, dbName))
 		if err != nil {
+			log.Println("Error:", err)
 			return err
 		}
 		return testDB.Ping()
 	}); err != nil {
-		pool.Purge(resource)
-		log.Fatalf("could not connect to db: %s", err)
+		_ = pool.Purge(resource)
+		log.Fatalf("could not connect to database: %s", err)
 	}
 
+	// populate the database with empty tables
+	err = createTables()
+	if err != nil {
+		log.Fatalf("error creating tables: %s", err)
+	}
+
+	// run tests
 	code := m.Run()
 
+	// clean up
+	if err = pool.Purge(resource); err != nil {
+		log.Fatalf("could not purge resource: %s", err)
+	}
+
 	os.Exit(code)
+}
+
+func createTables() error {
+	tableSQL, err := os.ReadFile("./testdata/movies.sql")
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	_, err = testDB.Exec(string(tableSQL))
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	return nil
 }
