aabd1ff2150c1aadf46c395b3b260c48bbc92ef6
petrostrak <pit.trak@gmail.com>
2024-01-20 11:43:02 +0200

    trying to add migrate to compose

--- migrations/000001_create_movies_table.up.sql
+++ migrations/000001_create_movies_table.up.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION citext;
+
 CREATE TABLE IF NOT EXISTS movies (
     id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(), 
